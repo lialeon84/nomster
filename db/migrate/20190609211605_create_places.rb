2c18efa379a3cf8eecbb47285988e73eef61ec78
Rosa <lealeon@att.net>
2019-06-10 02:03:10 +0000

    Add a good message describing what you actually changed since your last commit in here

--- db/migrate/20190609211605_create_places.rb
+++ db/migrate/20190609211605_create_places.rb
@@ -1,8 +1,7 @@
-class CreatePlaces < ActiveRecord::Migration[5.2]
+class CreatePlaces < ActiveRecord::Migration[5.0]
   def change
     create_table :places do |t|
-
-      t.string :name
+       t.string :name
       t.text :description
       t.string :address
 
