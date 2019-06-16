9ceae965bc20a7174324ec5e9e8805ce753b8846
Rosa <lealeon@att.net>
2019-06-16 01:50:40 +0000

    Add a good message describing what you actually changed since your last commit in here

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,10 +1,11 @@
 class Place < ApplicationRecord
 
   belongs_to :user
+  has_many :comments
 
   geocoded_by :address
   after_validation :geocode
-  
+
   validates :name, presence: true, length: {minimum: 3}
   validates :address, :description, presence: true
 
