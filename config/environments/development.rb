9c728697483cef5f51c35c7735c09f27d58d3957
Rosa <lealeon@att.net>
2019-06-29 16:48:52 +0000

    created sendgrid for heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
 
 #config.action_mailer.delivery_method = :test
    
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = :smtp
 
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
