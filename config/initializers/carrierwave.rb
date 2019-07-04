f1ccbd9878d7dea8eeba0e06c6a142df3aca1622
Rosa <lealeon@att.net>
2019-07-04 01:46:46 +0000

    updated aws key

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,9 +4,9 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     "AKIAUHBNPCLIRMM67POZ",       #["AWS_ACCESS_KEY"],        # required
-    aws_secret_access_key: "nziAZy8PNPbiQ5i9KaOvymNoyidAy4se/N2jw8H",                           #ENV["AWS_SECRET_KEY"],        # required
+    aws_access_key_id:      ENV["AWS_ACCESS_KEY"],         # required
+    aws_secret_access_key:  ENV["AWS_SECRET_KEY"],        # required
   }
-  config.fog_directory  =  "nomster-rose"                         #ENV["AWS_BUCKET"]              # required
+  config.fog_directory  = ENV["AWS_BUCKET"]              # required
 end
 
